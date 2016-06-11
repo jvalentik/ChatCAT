3ab8d390961b723e1ded8d95c315f89e57897fde
jvalentik@sk.ibm.com <SpringBoot3>
2016-06-12 00:07:14 +0200

    implemented router

---
+++ app/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Jan Valentik on 6/11/2016.
+ */
+'use strict';
+
+module.exports = {
+    router: require('./routes')()
+};
--- server.js
+++ server.js
@@ -4,13 +4,14 @@
 'use strict';
 const express = require('express');
 const app = express();
+const chatCat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-    res.render('login');
-});
+app.use('/', chatCat.router);
+
+
 
 app.listen(app.get('port'), () => console.log('ChatCAT running on Port: ', app.get('port')));
