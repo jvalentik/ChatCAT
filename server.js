e17f7213a9d5f4e9f46c37c14a9deef0a3d9c843
jvalentik@sk.ibm.com <SpringBoot3>
2016-06-11 22:49:19 +0200

    added EJS views

--- server.js
+++ server.js
@@ -2,5 +2,15 @@
  * Created by Jan Valentik on 6/11/2016.
  */
 'use strict';
+const express = require('express');
+const app = express();
 
-console.log("Welcome to ChatCat");
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    res.render('login');
+});
+
+app.listen(app.get('port'), () => console.log('ChatCAT running on Port: ', app.get('port')));
